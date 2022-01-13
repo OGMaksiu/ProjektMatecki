7d77ecaae6abed423c1c9e59ea073649008c30ec
student <OGMaksiu>
2022-01-13 13:11:28 +0100

    Comit

--- app/src/main/java/com/example/projektmatecki/MainActivity.kt
+++ app/src/main/java/com/example/projektmatecki/MainActivity.kt
@@ -12,10 +12,28 @@ class MainActivity : AppCompatActivity() {
 
     val next = findViewById<Button>(R.id.button)
     val previous = findViewById<Button>(R.id.button2)
-        val obraz1 = findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.oraz1)
-        val obraz2 = findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.obraz2)
-        val obraz3 = findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.obraz3)
-        val obraz4 = findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.obraz4)
+
+        var x = 1
+        findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.ic_launcher_background)
+
+        next.setOnClickListener{
+            if(x==1){
+                findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.oraz1)
+                x++
+        }
+            else if(x==2){
+                findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.obraz2)
+                x++
+            }
+            else if(x==3){
+                findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.obraz3)
+                x++
+            }
+            else if(x==4){
+                findViewById<ImageView>(R.id.imageView2).setImageResource(R.drawable.obraz4)
+                x = 1
+            }
+        }
 
 
 
